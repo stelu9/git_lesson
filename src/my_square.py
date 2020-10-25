6c335f6dd803326d6254a33d81a74f8e579898ba
Stelu <stelu9grama@gmail.com>
2020-10-25 15:43:23 +0000

    x int y and changed to 42

--- src/my_square.py
+++ src/my_square.py
@@ -1,6 +1,6 @@
-def my_square (x):
+def my_square (y):
 	"""takes a value and returns the square value"""
 	
 	"""uses the ** operator"""
-	return(x **2)
-print(my square(4))
+	return(y **2)
+print(my square(42))
